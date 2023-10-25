00cc201f1153f1462588c8a33f34500fcfec45a5
Gulshan <guptagulshan104@gmail.com>
2023-10-26 00:10:39 +0530

    react

---
+++ 01basicreact/src/Chai.js
@@ -0,0 +1,10 @@
+import Chai from "./chai.js"
+function chai() {
+    <>
+    return(
+        <Chai />
+  <h1>chai aur react never</h1>
+  </>
+    );
+}
+export default App;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+import Chai from "./App.jsx"
+function App() {
+   
+
+  return (
+   <h1>chai aur react with</h1>
+  )
+}
+
+export default App
